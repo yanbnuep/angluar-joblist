7ae3a2ebd021c9f5a2416de34517ff2559a693d6
wuiancocos2d <wuiancocos2d@gmail.com>
2018-07-01 23:41:26 +0800

    job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { JobsComponent } from './jobs/jobs.component';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatDividerModule} from '@angular/material/divider';
+import { AppRoutingModule } from './/app-routing.module';
+import { JobDetailComponent } from './job-detail/job-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent,
+    JobDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    AppRoutingModule,
+    MatDividerModule,
+    MatListModule,
   ],
+  exports: [RouterModule,MatTableModule,MatDividerModule,MatListModule],
   providers: [],
   bootstrap: [AppComponent]
 })
